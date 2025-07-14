f3e1b635ba946920bafd534f90649649d32cdb9f
MD ABDULLAH AL JEHAD <github000000001@gmail.com>
2025-07-14 12:53:52 +0600

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -10,10 +10,10 @@ logging.getLogger("urllib3").setLevel(logging.WARNING)
 
 
 class ENV_VARS(object):
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME")
+    API_ID = int(os.environ.get("API_ID", "25344449"))
+    API_HASH = os.environ.get("API_HASH", "24fd33596888304ec937b8130e73f48c")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8089615242:AAHih-ud-Mge9_OVIZjgQz_GP2HEFD6a6U0")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "MybotAnyting_bot")
     #AUTH_USER = int(os.environ.get("AUTH_USER", 5071059420))
 
 
